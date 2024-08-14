81e80a7d19a1440fbb008f3cd47d58f2d1e8088b
David Maiato <142182549+dmaiato@users.noreply.github.com>
2024-08-14 16:02:11 -0300

    Update gerador.java

--- trabalho2/GeradorMne/gerador.java
+++ trabalho2/GeradorMne/gerador.java
@@ -1,21 +1,16 @@
 package GeradorMne;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class gerador {
   
   static String geradorMne (String texto){
     String senha = "";
 
-    List<String> lista = List.of(texto.split(" "));
+    String[] lista = texto.split(" ");
 
     for (String i: lista) {
       senha += i.charAt(0);
     }
 
     return senha;
-
   }
-
 }
